5737465593516de6776c25dd9d5edac588d185ec
Sokolov Innokenty <r2.kenny@gmail.com>
2011-06-24 01:13:05 +0400

    add Abstract transport

---
+++ Transport/Abstract.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Abstract
+ *
+ * @package Q_Mail
+ * @author Sokolov Innokenty, <sokolov.innokenty@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @copyright Copyright (c) 2011, qbbr
+ */
+abstract class Q_Mail_Transport_Abstract
+{
+}
